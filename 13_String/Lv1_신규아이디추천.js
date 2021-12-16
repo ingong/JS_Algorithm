ed93a48faff6a2df46436b6d6b03b122acd9295c
ingong <insong1216@gmail.com>
2021-12-16 19:23:44 +0900

    신규아이디 추천

---
+++ 13_String/Lv1_신규아이디추천.js
@@ -0,0 +1,11 @@
+function solution(new_id) {
+  const answer = new_id
+    .toLowerCase()
+    .replace(/[^\w-_.]/g, '')
+    .replace(/\.+/g, '.')
+    .replace(/^\.|\.$/g, '')
+    .replace(/^$/, 'a')
+    .slice(0, 15)
+    .replace(/\.$/, '');
+  return answer.padEnd(3, answer[answer.length - 1]);
+}
